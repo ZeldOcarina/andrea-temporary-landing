685ed2a842db316ec71ed83cf5ff85f8c3003594
ZeldOcarina <mattia.r86@gmail.com>
2021-08-26 12:20:14 +0200

    Fix broken description meta tag

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -3,10 +3,8 @@ import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
 const SEO = ({ title, description, language }) => {
-  const metaTags = [{ name: `description`, content: description }];
-
   return (
-    <Helmet title={title || "Andrea d'Agostini"} meta={metaTags} htmlAttributes={{ lang: language }}>
+    <Helmet title={title || "Andrea d'Agostini"} htmlAttributes={{ lang: language }}>
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -14,6 +12,7 @@ const SEO = ({ title, description, language }) => {
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
+      <meta name="description" content={description} />
     </Helmet>
   );
 };
